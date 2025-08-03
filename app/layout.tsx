d89a34ac233f5aed10d5f0de90be21dd6ca323f5
bradleydoering <bradley.doering@gmail.com>
2025-08-03 12:21:16 -0700

    Trigger Vercel redeploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "CloudReno",
+  title: "CloudReno", 
   description: "CloudReno - Professional Bathroom Renovation Packages",
 };
 
